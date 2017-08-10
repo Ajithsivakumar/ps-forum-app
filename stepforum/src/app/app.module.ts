45ff201cddad8a1257d866e3b5f9ff489f044cec
Ajithsivakumar <ajithsivakumar321@gmail.com>
2017-08-10 10:27:14 +0530

    dashboard component

--- stepforum/src/app/app.module.ts
+++ stepforum/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginformComponent } from './loginform/loginform.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginformComponent
+    LoginformComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
