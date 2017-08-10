6755a6f9aa0cd06df5bdf5ffd0d781c96bf23448
Ajithsivakumar <ajithsivakumar321@gmail.com>
2017-08-10 11:19:05 +0530

    router navigation
    navigation from login form to dashboard

--- stepforum/src/app/app.module.ts
+++ stepforum/src/app/app.module.ts
@@ -6,7 +6,17 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginformComponent } from './loginform/loginform.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import {RouterModule,Routes} from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:'',
+    component:LoginformComponent
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent
+  }
+  ]
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +26,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- stepforum/src/app/loginform/loginform.component.ts
+++ stepforum/src/app/loginform/loginform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginformComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+loginuser(e)
+{
+  e.preventDefault();
+  console.log(e);
+  var name=e.target.elements[0].value;
+  var pass=e.target.elements[1].value;
+  if(name=="admin"&&pass=="admin")
+    {
+      this.router.navigate(['dashboard']);
+    }
+}
 }
