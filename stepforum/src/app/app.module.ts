d1a41dd684484da436b6b33f465fee26350def92
Ajithsivakumar <ajithsivakumar321@gmail.com>
2017-08-10 16:01:38 +0530

    app module changes

--- stepforum/src/app/app.module.ts
+++ stepforum/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { LoginformComponent } from './loginform/loginform.component';
+import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {RouterModule,Routes} from '@angular/router';
 import { ListComponent } from './list/list.component';
+import { HttpModule } from "@angular/http";
 const appRoutes:Routes=[
   {
     path:'',
@@ -21,19 +22,21 @@ const appRoutes:Routes=[
     path:'list',
     component:ListComponent
   }
-  ]
+]
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     LoginformComponent,
+    FooterComponent,
     DashboardComponent,
     ListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
